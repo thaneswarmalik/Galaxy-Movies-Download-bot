1dab8434589ed8b824d2f7584f97831d1a4c8e98
thaneswarmalik <157218198+thaneswarmalik@users.noreply.github.com>
2024-01-26 13:01:16 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = os.getenv("URL")
+URL = os.getenv("https://movies-download-bot.onrender.com")
 bot = Bot(TOKEN)
 
 
